39114f7d2b80f09e977ef537565e37d83e6a37c0
yesutkarsh <contactutkarshverma@gmail.com>
2025-06-15 23:18:18 +0530

    Count digits

---
+++ #DAY 2 Count Digits/index.js
@@ -0,0 +1,12 @@
+// count Number of Digits 
+// let n = 122;
+// let count = 0;
+
+
+// while(n !==0){
+//     if(n == 0) count++
+//     n = Math.floor(n/10)
+//     count++
+// }
+
+// console.log(count)
