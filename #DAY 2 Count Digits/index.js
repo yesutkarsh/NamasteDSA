5a0cd6d94e0a1ff8f52eb7661d8c2ba5776707a2
yesutkarsh <contactutkarshverma@gmail.com>
2025-06-15 23:52:24 +0530

    Palindrome

--- #DAY 2 Count Digits/index.js
+++ #DAY 2 Count Digits/index.js
@@ -10,3 +10,43 @@
 // }
 
 // console.log(count)
+
+
+
+
+// count Number of Digits 
+// let n = 122;
+// let count = 0;
+
+
+// while(n !==0){
+//     if(n == 0) count++
+//     n = Math.floor(n/10)
+//     count++
+// }
+
+// console.log(count)
+
+
+
+
+// Palindrome
+// let n2 = 121;
+// let n3 = -123
+
+// function checkPalinDrome(n){
+
+//     if(n<0) return false
+//     const og = n;
+//     let newNum = "";
+//     n = Math.abs(n)
+
+//     while(n>0){
+//         newNum += n%10
+//         n = Math.floor(n/10);
+//     }
+
+//     return og == newNum
+// }
+
+// console.log(checkPalinDrome(n3)) 
